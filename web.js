e4ec2d2a47bbc15baee6c88c62d05a52a8331998
Joe Goldbeck <goldbeck.joseph@gmail.com>
2015-09-24 11:34:25 -0400

    handle bare url

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express();
 var statusCode  = parseInt(process.env.STATUS_CODE) || 301;
 
 app.get('*', function(request, response) {
-  response.redirect(statusCode, process.env.NEW_BASE_URL + request.url)
+  response.redirect(statusCode, process.env.NEW_BASE_URL + (request.url || ''))
 });
 
 var port = process.env.PORT || 5000;
